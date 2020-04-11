28ac0e966a56d2696f87d1d9b7d8d5fc958c57ea
Roushan Kumar <roushan.kumar@sigmoidanalytics.com>
2020-04-12 01:06:58 +0530

    Repositories - Employee, Customer, Pet and Schedule

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -0,0 +1,20 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findCustomerById(Long customerId);
+
+//    @Query("update Customer c set c.pets = :pets where c.id = :customerId")
+//    void addPetsToCustomer(List<Pet> pets, Long customerId);
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Employee findEmployeeById(Long employeeId);
+}
